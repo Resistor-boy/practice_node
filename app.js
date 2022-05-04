ad8a3b0e6fecc2139193bbd8217cec5bdd742bf3
Resister-boy <jaehun4122@gmail.com>
2022-05-04 16:22:14 +0900

    First_Storagy

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// MODULE
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+//SETTING APP
+app.set('views', './views');
+app.set('view engine', 'ejs')
+
+// ROUTER
+const home = require('./routes/home')
+app.use('/', home); // use => Middle웨어를 등록해주는 Method!
+
+app.listen(PORT, () => {
+    console.log(`Server is Listening at ${PORT}!!`);
+});
+
+module.exports = app;
